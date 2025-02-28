46155e55808ab91fbb48178587090ec8db81d7de
gokulprasanthb <gokulprasanthbm@gmail.com>
2025-02-28 09:48:05 +0530

    index file create

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const express = require('express');
+const route  = require('./Router/router');
+
+const app = express();
+app.use(express.json());
+
+app.use('/', route);
+
+app.listen(5050, ()=>{console.log("running at port 5050");})
+
