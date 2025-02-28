53911c9e7aed833d1da9aa070fe3d99a885be285
gokulprasanthb <gokulprasanthbm@gmail.com>
2025-02-28 09:53:35 +0530

    student list api

--- server/Controller/controller.js
+++ server/Controller/controller.js
@@ -24,4 +24,16 @@ async function getEventList(req,res){
     }
 };
 
-module.exports = {getSpecialLabList, getEventList};
+async function getStudentList(req,res){
+    try {   
+        const id = req.params.id;
+        const query = `select roll_number, student_name, email, department_name from student_list where student_list_id = ${id};`; 
+        const response = await runQuery(query);
+        const studentData = response.recordset[0];
+        res.status(200).send(studentData);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+module.exports = {getSpecialLabList, getEventList, getStudentList};
--- server/Router/router.js
+++ server/Router/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getSpecialLabList, getEventList} = require('../Controller/controller');
+const { getSpecialLabList, getEventList, getStudentList} = require('../Controller/controller');
 
 const route = express.Router();
 
 route.get('/SpecialLabList/:id', getSpecialLabList);
 route.get('/EventList/:id', getEventList);
+route.get('/StudentList/:id', getStudentList);
 
 module.exports = route;
